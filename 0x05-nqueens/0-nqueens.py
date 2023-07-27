93a39457102ef6b7b79ccf56f6e43cb1d4ad9ab1
ENDALE LEGESSE <101863853+Endale24@users.noreply.github.com>
2023-07-27 20:44:28 +0800

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,95 +3,69 @@
 """
 import sys
 
+def print_solution(board, N):
+    solution = []
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                solution.append(j+1)
+    print(", ".join(str(x) for x in solution))
 
-def nqueens(num):
-    """nqueens function that solves the N queens problem
+def is_attack(board, row, col, N):
+    for i in range(col):
+        if board[row][i] == 1:
+            return True
 
-    Args:
-        num (int): number of queens
-    """
-    board = [[0 for x in range(num)] for y in range(num)]
-    solve_nqueens(board, 0, num)
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return True
 
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return True
 
-def solve_nqueens(board, col, num):
-    """solve_nqueens function that solves the N queens problem
+    return False
 
-    Args:
-        board (list): board list
-        col (int): column number
-        num (int): number of queens in board
-
-    Returns:
-        bool: True if safe, False otherwise
-    """
-    if col == num:
-        print_board(board)
+def solve_NQ(board, col, N):
+    if col >= N:
+        print_solution(board, N)
         return True
 
     res = False
-    for i in range(num):
-        if is_safe(board, i, col, num):
+    for i in range(N):
+        if not is_attack(board, i, col, N):
             board[i][col] = 1
-            res = solve_nqueens(board, col + 1, num) or res
-            board[i][col] = 0
+
+            res = solve_NQ(board, col + 1, N) or res
+
+            board[i][col] = 0  # backtrack
+
     return res
 
+def solveNQ(N):
+    board = [[0]*N for _ in range(N)]
 
-def is_safe(board, row, col, num):
-    """is_safe function that checks if a queen can be placed on board
+    if not solve_NQ(board, 0, N):
+        print("Solution does not exist")
+        return False
 
-    Args:
-        board (list): board list
-        row (int): row number
-        col (int): column number
-        num (int): number of queens in board
-
-    Returns:
-        bool: True if safe, False otherwise
-    """
-    safe_row = row
-    safe_col = col
-    while safe_col >= 0 and safe_row < num:
-        if board[safe_row][safe_col] == 1:
-            return False
-        safe_row += 1
-        safe_col -= 1
-
-    safe_row = row
-    safe_col = col
-    while safe_row >= 0 and safe_col >= 0:
-        if board[safe_row][safe_col] == 1:
-            return False
-        safe_row -= 1
-        safe_col -= 1
-
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
     return True
 
-
-def print_board(board):
-    """print_board function that prints the board
-
-    Args:
-        board (list): board list
-    """
-    print([[i, j] for i in range(
-        len(board)) for j in range(len(board)) if board[i][j] == 1])
-
-
-if __name__ == "__main__":
+def main():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     try:
-        NUM = int(sys.argv[1])
-        if NUM < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-        nqueens(NUM)
+        N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solveNQ(N)
+
+if __name__ == "__main__":
+    main()
