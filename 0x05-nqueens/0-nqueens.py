57ba62b1821b1ab43908ed7ce77bb49bb7bed66d
ENDALE LEGESSE <101863853+Endale24@users.noreply.github.com>
2023-07-31 18:56:45 +0800

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,68 +1,57 @@
 #!/usr/bin/python3
-"""0-nqueens module that solves the N queens problem
-"""
+
 import sys
-from operator import attrgetter
-import random
-from chromosome import Chromosome
 
-class NQueens:
-    def __init__(self, dimension, population_count, mutation_factor, iteration):
-        self.mutation_factor = mutation_factor
-        self.population_count = population_count
-        self.dimension = dimension
-        self.iteration = iteration
-        self.population = []
 
-        for n in range(0, population_count):
-            chromosome = Chromosome(dimension)
-            self.population.append(chromosome)
+def solve(row, column):
+    solver = [[]]
+    for q in range(row):
+        solver = place_queen(q, column, solver)
+    return solver
 
-    def total_fitness(self):
-        return sum([chromosome.fitness for chromosome in self.population])
 
-    def weighted_random_choice(self, choices):
-        max = sum(choices.values())
-        pick = random.uniform(0, max)
-        current = 0
-        for key, value in choices.items():
-            current += value
-            if current > pick:
-                return key
+def place_queen(q, column, prev_solver):
+    solver_queen = []
+    for array in prev_solver:
+        for x in range(column):
+            if is_safe(q, x, array):
+                solver_queen.append(array + [x])
+    return solver_queen
 
-    # Choose better fitness using roulette wheels
-    def select(self):
-        random.shuffle(self.population)
-        new_population = []
-        choices = {chromosome: chromosome.fitness for chromosome in self.population}
-        for i in range(0, self.population_count):
-            new_population.append(self.weighted_random_choice(choices))
-        self.population = new_population
 
-    def crossover(self):
-        for i in range(0, len(self.population) if len(self.population) % 2 == 0 else len(self.population) - 1, 2):
-            point = random.choice(range(0, self.dimension))
-            parent_right1 = self.population[i].genes[point:self.dimension]
-            parent_right2 = self.population[i + 1].genes[point:self.dimension]
-            chromosome1 = self.population[i]
-            chromosome2 = self.population[i + 1]
-            chromosome1.genes[point:self.dimension] = parent_right2
-            chromosome2.genes[point:self.dimension] = parent_right1
-            self.population.extend([chromosome1, chromosome2])
+def is_safe(q, x, array):
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
 
-    def mutate(self):
-        for chromosome in self.population:
-            if random.random() < self.mutation_factor:
-                chromosome.genes[random.randint(0, self.dimension - 1)] = random.randint(0, self.dimension - 1)
 
-    def solve(self):
-        for n in range(0, self.iteration):
-            self.select()
-            self.crossover()
-            self.mutate()
-            maximum_chromosome = max(self.population, key=attrgetter('fitness'))
-            maximum = maximum_chromosome.fitness
-            print 'Generation=>', n + 1, 'Maximum Fitness=>', maximum
-            if maximum == (self.dimension * (self.dimension - 1)) / 2:
-                print maximum_chromosome.genes, 'Fitness=>', maximum
-                break
+def init():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit():
+        the_queen = int(sys.argv[1])
+    else:
+        print("N must be a number")
+        sys.exit(1)
+    if the_queen < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return(the_queen)
+
+
+def n_queens():
+
+    the_queen = init()
+    solver = solve(the_queen, the_queen)
+    for array in solver:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
+
+
+if __name__ == '__main__':
+    n_queens()
