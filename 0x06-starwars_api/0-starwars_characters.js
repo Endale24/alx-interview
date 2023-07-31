0a140d432b589c74425d0cbe471d39d3e89daa46
ENDALE LEGESSE <101863853+Endale24@users.noreply.github.com>
2023-07-31 18:58:50 +0800

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
-
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-request(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  }
-  const characters = JSON.parse(body).characters;
-  const promises = characters.map((character) => {
-    return new Promise((resolve, reject) => {
-      request(character, (err, res, body) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(JSON.parse(body).name);
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[character], (err, res, html) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
       });
-    });
+      console.log(res);
+    }
   }
-  );
-  Promise.all(promises).then((names) => {
-    names.forEach((name) => {
-      console.log(name);
-    });
-  }
-  );
 });
